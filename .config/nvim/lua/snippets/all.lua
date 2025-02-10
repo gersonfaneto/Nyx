cb5ca2bf517fc7db31e4e2846b53a7f9b4b4e683
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-02-10 16:34:06 -0300

    feat(nvim): add `all` snippet -> `vimopt`

--- .config/nvim/lua/snippets/all.lua
+++ .config/nvim/lua/snippets/all.lua
@@ -1,7 +1,13 @@
 local M = {}
+local un = require('utils.snippets.nodes')
 local us = require('utils.snippets.snips')
 local ls = require('luasnip')
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
 local f = ls.function_node
+local d = ls.dynamic_node
+local c = ls.choice_node
 
 M.snippets = {
   us.sn(
@@ -12,6 +18,28 @@ M.snippets = {
     f(function()
       return vim.trim(vim.system({ 'date' }):wait().stdout)
     end)
+  ),
+  us.sn(
+    {
+      trig = 'vimopt',
+      desc = 'Vim options for the current file',
+    },
+    un.fmtad('<commentstring>vim: <options> :', {
+      commentstring = d(2, function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local str = vim.fn.trim(
+          vim.api.nvim_get_option_value('commentstring', { buf = bufnr }),
+          '%s',
+          2
+        )
+
+        return sn(nil, t(str))
+      end),
+      options = c(1, {
+        i(1),
+        t('sw=4 ts=4 sts=4 et tw=80 nospell'),
+      }),
+    })
   ),
 }
 
