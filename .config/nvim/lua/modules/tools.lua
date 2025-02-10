2a3202bef1af3ed40abb28698d7ca0aa35092e9a
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-02-10 19:05:44 -0300

    refactor(nvim): remove plugin - `harpoon`

--- .config/nvim/lua/modules/tools.lua
+++ .config/nvim/lua/modules/tools.lua
@@ -1,14 +1,5 @@
 return {
   {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('configs.harpoon')
-    end,
-  },
-
-  {
     'ibhagwan/fzf-lua',
     cmd = 'FzfLua',
     keys = {
