5ef76e9d277c143d14a39e52748f431f08a26a39
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-03-30 10:05:17 -0300

    feat(nvim): add `EZ Semicolon` autocmd

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -535,3 +535,70 @@ augroup('ColorSchemeRestore', {
     end,
   },
 })
+
+augroup('EZSemicolon', {
+  'InsertEnter',
+  {
+    once = true,
+    desc = 'Implements `EZ Semicolon` (VS Code extension) functionality',
+    pattern = {
+      '*.rs',
+      '*.c',
+      '*.cpp',
+      '*.ts',
+      '*.js',
+      '*.tsx',
+      '*.jsx',
+      '*.css',
+      '*.java',
+      '*.cs',
+      '*.php',
+      '*.ml',
+    },
+    callback = function()
+      local function trim(s)
+        return s:gsub('^%s+', ''):gsub('%s+$', '')
+      end
+
+      local function trimBeg(s)
+        return s:gsub('^%s+', '')
+      end
+
+      local function isInFor(s)
+        local firstFour = string.sub(trimBeg(s), 1, 4)
+        return firstFour == 'for ' or firstFour == 'for('
+      end
+
+      local function isInReturn(s)
+        local firstSeven = string.sub(trimBeg(s), 1, 7)
+        return firstSeven == 'return '
+          or firstSeven == 'return'
+          or firstSeven == 'return;'
+      end
+
+      vim.keymap.set('i', ';', function()
+        local line = vim.api.nvim_get_current_line()
+        local last = string.sub(trim(line), -1)
+        if isInFor(line) then
+          return ';'
+        end
+        if isInReturn(line) then
+          if last == ';' then
+            return '<Esc>g_'
+          else
+            return '<Esc>g_a;<Esc>'
+          end
+        end
+        if last == ';' then
+          return '<Esc>$a<CR>'
+        else
+          if string.len(trim(line)) == 0 then
+            return ';<Esc>==$a<CR>'
+          end
+          return '<Esc>g_a;'
+        end
+      end, { expr = true })
+      vim.keymap.set('i', '<M-;>', ';', { remap = false })
+    end,
+  },
+})
