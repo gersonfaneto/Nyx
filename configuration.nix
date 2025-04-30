c7b405d30dc394a29e8d572f0a50318dc9370aa6
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-04-29 21:23:18 -0300

    feat(nix): configure enviroment variables

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,21 @@ in
     };
   };
 
+  environment.variables = rec {
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME    = "$HOME/.local/bin";
+    PATH = [ 
+      "${XDG_BIN_HOME}"
+    ];
+
+    LM_LICENSE_FILE = "$HOME/.licenses/LR-231669_License.dat";
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
