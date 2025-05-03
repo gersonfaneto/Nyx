10dfec89f06359d65698eb34407feaee2df9d2dc
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-05-03 14:48:48 -0300

    fix(nvim): check if jdtls is avaliable in the PATH before setup

--- .config/nvim/after/ftplugin/java/lsp.lua
+++ .config/nvim/after/ftplugin/java/lsp.lua
@@ -1,3 +1,7 @@
+if vim.fn.executable('jdtls') == 0 then
+  return
+end
+
 local jdtls = require('jdtls')
 
 local config = {
@@ -6,47 +10,16 @@ local config = {
     vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]
   ),
 }
+
 jdtls.start_or_attach(config)
 
 local opts = { silent = true, buffer = 0 }
 
-vim.keymap.set(
-  { 'n' },
-  '<localleader>oi',
-  "<Cmd>lua require('jdtls').organize_imports()<CR>",
-  opts
-)
-
-vim.keymap.set(
-  { 'n' },
-  '<localleader>ev',
-  "<Cmd>lua require('jdtls').extract_variable()<CR>",
-  opts
-)
-vim.keymap.set(
-  { 'x' },
-  '<localleader>ev',
-  "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
-  opts
-)
-
-vim.keymap.set(
-  { 'n' },
-  '<localleader>ec',
-  "<Esc><Cmd>lua require('jdtls').extract_constant()<CR>",
-  opts
-)
-vim.keymap.set(
-  { 'x' },
-  '<localleader>ec',
-  "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
-  opts
-)
-
-vim.keymap.set(
-  { 'x' },
-  '<localleader>em',
-  "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
-  opts
-)
-
+-- stylua: ignore start
+vim.keymap.set({ 'n' }, '<localleader>oi', "<Cmd>lua require('jdtls').organize_imports()<CR>", opts)
+vim.keymap.set({ 'n' }, '<localleader>ev', "<Cmd>lua require('jdtls').extract_variable()<CR>", opts)
+vim.keymap.set({ 'x' }, '<localleader>ev', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
+vim.keymap.set({ 'n' }, '<localleader>ec', "<Esc><Cmd>lua require('jdtls').extract_constant()<CR>", opts)
+vim.keymap.set({ 'x' }, '<localleader>ec', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", opts)
+vim.keymap.set( { 'x' }, '<localleader>em', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
+-- stylua: ignore off
