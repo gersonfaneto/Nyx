cbb5d552017c9a9bf52c304285cbc4110d2c7bc4
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-05-06 09:29:23 -0300

    feat(tmux): add preview to `fzf-find-window`

--- .config/tmux/scripts/fzf-find-window.sh
+++ .config/tmux/scripts/fzf-find-window.sh
@@ -6,20 +6,27 @@
 # set -e
 # set -x
 
+# shellcheck disable=SC2016
 tmux list-windows -aF '#S:#I (#W)' \
     | sed -e 's/^\([^:]*\):/\x1b[34m\1\x1b[0m:/' \
         -e 's/:\([0-9]\+\) /\:\x1b[32m\1\x1b[0m /' \
         -e 's/(\(.*\))$/(\x1b[33m\1\x1b[0m)/' \
-    | fzf --no-preview --ansi \
+    | fzf --ansi \
         --color hl:reverse:-1,hl+:reverse:-1 \
-        --prompt 'Tmux windows> ' \
+        --prompt 'Windows :: ' \
         --border=rounded \
+        --preview '
+            sel=$(echo {} | sed "s/([^()]*)//g" | awk "{print \$1}")
+            tmux capture-pane -t $sel -ep -S -50
+        ' \
+        --preview-window=up:50%:noinfo \
+        --no-preview-label \
         --tmux "center,$(
             w=$(tmux display -p '#{window_width}')
-            echo $((w < 64 ? w : 64))
+            echo $((w < 64 ? w : 80))
         ),$(
             h=$(tmux display -p '#{window_height}')
-            echo $((h < 16 ? h : 16))
+            echo $((h < 16 ? h : 32))
         )" \
         --bind "ctrl-x:execute(echo {+} | \
         sed 's/([^()]*)//g' | \
