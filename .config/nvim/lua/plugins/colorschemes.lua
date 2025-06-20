3a3b48b7d83e6dc2f5c5fa8aa8d1817a35b5efbb
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-06-20 08:56:27 -0300

    X

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,4 +1,26 @@
 return {
-  { 'sainnhe/everforest' },
-  { 'sainnhe/gruvbox-material' },
+  {
+    'sainnhe/everforest',
+    init = function()
+      vim.g.everforest_transparent_background = 0
+      vim.g.everforest_background = 'hard'
+      vim.g.everforest_ui_contrast = 'high'
+      vim.g.everforest_float_style = 'bright'
+      vim.g.everforest_cursor = 'auto'
+      vim.g.everforest_better_performance = 1
+    end,
+  },
+  {
+    'sainnhe/gruvbox-material',
+    init = function()
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_foreground = 'mix'
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_ui_contrast = 'high'
+      vim.g.gruvbox_material_float_style = 'bright'
+      vim.g.gruvbox_material_statusline_style = 'material'
+      vim.g.gruvbox_material_cursor = 'auto'
+      vim.g.gruvbox_material_better_performance = 1
+    end,
+  },
 }
