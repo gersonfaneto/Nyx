987def5db79410504fb676da9854a76b4fd8ecfc
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-07-11 08:09:38 -0300

    X

--- .config/nvim/lua/utils/opts.lua
+++ .config/nvim/lua/utils/opts.lua
@@ -1,12 +1,11 @@
 local M = {}
 
----See `:h 'quickfixtextfunc'`
----@param info table
+---@param args table
 ---@return string[]
-function M.qftf(info)
-  local qflist = info.quickfix == 1
-      and vim.fn.getqflist({ id = info.id, items = 0 }).items
-    or vim.fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+function M.qftf(args)
+  local qflist = args.quickfix == 1
+      and vim.fn.getqflist({ id = args.id, items = 0 }).items
+    or vim.fn.getloclist(args.winid, { id = args.id, items = 0 }).items
 
   if vim.tbl_isempty(qflist) then
     return {}
@@ -152,145 +151,6 @@ function M.qftf(info)
   end
 
   return lines
-end
-
--- Text object: folds
----Returns the key sequence to select around/inside a fold,
----supposed to be called in visual mode
----@param motion 'i'|'a'
----@return string
-function M.textobj_fold(motion)
-  local lnum = vim.fn.line('.') --[[@as integer]]
-  local sel_start = vim.fn.line('v')
-  local lev = vim.fn.foldlevel(lnum)
-  local levp = vim.fn.foldlevel(lnum - 1)
-  -- Multi-line selection with cursor on top of selection
-  if sel_start > lnum then
-    return (lev == 0 and 'zk' or lev > levp and levp > 0 and 'k' or '')
-      .. vim.v.count1
-      .. (motion == 'i' and ']zkV[zj' or ']zV[z')
-  end
-  return (lev == 0 and 'zj' or lev > levp and 'j' or '')
-    .. vim.v.count1
-    .. (motion == 'i' and '[zjV]zk' or '[zV]z')
-end
-
----Go to the first line of current paragraph
----@return nil
-function M.goto_paragraph_firstline()
-  local chunk_size = 10
-  local linenr = vim.fn.line('.')
-  local count = vim.v.count1
-
-  -- If current line is the first line of paragraph, move one line
-  -- upwards first to goto the first line of previous paragraph
-  if linenr >= 2 then
-    local lines = vim.api.nvim_buf_get_lines(0, linenr - 2, linenr, false)
-    if lines[1]:match('^$') and lines[2]:match('%S') then
-      linenr = linenr - 1
-    end
-  end
-
-  while linenr >= 1 do
-    local chunk = vim.api.nvim_buf_get_lines(
-      0,
-      math.max(0, linenr - chunk_size - 1),
-      linenr - 1,
-      false
-    )
-    for i, line in ipairs(vim.iter(chunk):rev():totable()) do
-      local current_linenr = linenr - i
-      if line:match('^$') then
-        count = count - 1
-        if count <= 0 then
-          vim.cmd.normal({ "m'", bang = true })
-          vim.cmd(tostring(current_linenr + 1))
-          return
-        end
-      elseif current_linenr <= 1 then
-        vim.cmd.normal({ "m'", bang = true })
-        vim.cmd('1')
-        return
-      end
-    end
-    linenr = linenr - chunk_size
-  end
-end
-
----Go to the last line of current paragraph
----@return nil
-function M.goto_paragraph_lastline()
-  local chunk_size = 10
-  local linenr = vim.fn.line('.')
-  local buf_line_count = vim.api.nvim_buf_line_count(0)
-  local count = vim.v.count1
-
-  -- If current line is the last line of paragraph, move one line
-  -- downwards first to goto the last line of next paragraph
-  if buf_line_count - linenr >= 1 then
-    local lines = vim.api.nvim_buf_get_lines(0, linenr - 1, linenr + 1, false)
-    if lines[1]:match('%S') and lines[2]:match('^$') then
-      linenr = linenr + 1
-    end
-  end
-
-  while linenr <= buf_line_count do
-    local chunk =
-      vim.api.nvim_buf_get_lines(0, linenr, linenr + chunk_size, false)
-    for i, line in ipairs(chunk) do
-      local current_linenr = linenr + i
-      if line:match('^$') then
-        count = count - 1
-        if count <= 0 then
-          vim.cmd.normal({ "m'", bang = true })
-          vim.cmd(tostring(current_linenr - 1))
-          return
-        end
-      elseif current_linenr >= buf_line_count then
-        vim.cmd.normal({ "m'", bang = true })
-        vim.cmd(tostring(buf_line_count))
-        return
-      end
-    end
-    linenr = linenr + chunk_size
-  end
-end
-
----Close floating windows with a given key, supposed to be used in a keymap
---- 1. If current window is a floating window, close it and return
---- 2. Else, close all floating windows that can be focused
---- 3. Fallback to `key` if no floating window can be focused
----@param key string
----@return nil
-function M.close_floats_keymap(key)
-  local current_win = vim.api.nvim_get_current_win()
-
-  -- Only close current win if it's a floating window
-  if vim.fn.win_gettype(current_win) == 'popup' then
-    vim.api.nvim_win_close(current_win, true)
-    return
-  end
-
-  -- Else close all focusable floating windows in current tab page
-  local win_closed = false
-  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-    if
-      vim.fn.win_gettype(win) == 'popup'
-      and vim.api.nvim_win_get_config(win).focusable
-    then
-      vim.api.nvim_win_close(win, false) -- do not force
-      win_closed = true
-    end
-  end
-
-  -- If no floating window is closed, fallback
-  if not win_closed then
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes(key, true, true, true),
-      'n',
-      false
-    )
-  end
 end
 
 return M
