8c6ee4fd6a77954c60310f4757ef87130f32a53c
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-07-15 09:22:27 -0300

    X

--- .config/fish/conf.d/zoxide.fish
+++ .config/fish/conf.d/zoxide.fish
@@ -2,6 +2,7 @@ if not status is-interactive
     exit
 end
 
-if type -q zoxide
+# Setup zoxide if z.fish is not available
+if type -q zoxide; and not type -q __z
     zoxide init fish | source
 end
