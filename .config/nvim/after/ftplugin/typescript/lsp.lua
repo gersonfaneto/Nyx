a0559c698e918c28a20c6052b61dcb1537f47228
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-07-18 18:26:39 -0300

    X

--- .config/nvim/after/ftplugin/typescript/lsp.lua
+++ .config/nvim/after/ftplugin/typescript/lsp.lua
@@ -7,11 +7,9 @@
 -- - jsonc
 -- - html
 -- - css
--- - tailwindcss
 
 vim.lsp.enable('typescript-language-server')
 vim.lsp.enable('vtsls')
 vim.lsp.enable('eslint')
 vim.lsp.enable('prettier')
 vim.lsp.enable('biome') -- prefer biome over prettier as formatter
-vim.lsp.enable('tailwindcss')
