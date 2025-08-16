0bb21d51d6ce5c25e26fd9bc11df90cc83474ba1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-08-16 08:42:58 -0300

    X

--- .config/nvim/lua/plugins/langs.lua
+++ .config/nvim/lua/plugins/langs.lua
@@ -13,4 +13,34 @@ return {
     'Vimjas/vim-python-pep8-indent',
     ft = 'python',
   },
+  {
+    'Olical/conjure',
+    ft = { 'clojure' },
+    dependencies = {
+      {
+        'Saghen/blink.compat',
+      },
+      {
+        'PaterJason/cmp-conjure',
+      },
+      {
+        'saghen/blink.cmp',
+        opts = {
+          sources = {
+            per_filetype = {
+              clojure = {
+                'conjure',
+              },
+            },
+            providers = {
+              conjure = {
+                name = 'conjure',
+                module = 'blink.compat.source',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
