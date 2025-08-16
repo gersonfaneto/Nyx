61250978c0c49adfaf63019e2f67b48bebfd97af
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-08-16 08:06:48 -0300

    X

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -261,27 +261,6 @@ vim.api.nvim_create_autocmd('UIEnter', {
     map('n', '<Leader>.', '<Cmd>FZF<CR>', { desc = 'Find files' })
     map('n', '<Leader>ff', '<Cmd>FZF<CR>', { desc = 'Find files' })
 
-
-    -- stylua: ignore start
-    map('n', '<Leader>uw', '<Cmd>set wrap!<CR>', { desc = 'Toggle word wrapping' })
-    map('n', '<Leader>us', '<Cmd>set spell!<CR>', { desc = 'Toggle spell checking' })
-    map('n', '<Leader>ur', '<Cmd>set relativenumber!<CR>', { desc = 'Toggle relative line numbers' })
-    map('n', '<Leader>uc', function ()
-      local conceallevel = vim.api.nvim_get_option_value('conceallevel', {})
-      vim.api.nvim_set_option_value('conceallevel', conceallevel == 0 and 3 or 0, {})
-    end, { desc = 'Toggle pell checking' })
-    map('n', '<Leader>ub', function()
-      local background = vim.api.nvim_get_option_value('background', {})
-      vim.api.nvim_set_option_value('background', background == 'dark' and 'light' or 'dark', {})
-    end, { desc = 'Toggle background color' })
-    -- stylua: ignore end
-
-    -- stylua: ignore start
-    map('n', '<Leader>bw', '<Cmd>write<CR>', { desc = 'Write' })
-    map('n', '<Leader>bq', '<Cmd>quit<CR>', { desc = 'Quit' })
-    map('n', '<Leader>bd', '<Cmd>bdelete<CR>', { desc = 'Delete' })
-    -- stylua: ignore end
-
     -- Abbreviations
     map('!a', 'ture', 'true')
     map('!a', 'Ture', 'True')
