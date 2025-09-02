6477497128ea95e51aabe39bfa14beb19ef4e036
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-09-02 18:15:35 -0300

    X

--- .config/nvim/after/lsp/jdtls.lua
+++ .config/nvim/after/lsp/jdtls.lua
@@ -28,7 +28,10 @@ return {
   },
   filetypes = { 'java' },
   root_markers = {
-    { 'build.gradle', 'build.gradle.kts' },
+    {
+      'build.gradle',
+      'build.gradle.kts',
+    },
     {
       'build.xml',
       'pom.xml',
