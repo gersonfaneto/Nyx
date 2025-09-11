bb61ba202d95209b4a626fdc0181baac74051dcc
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-09-11 19:59:38 -0300

    X

--- .config/nvim/after/ftplugin/markdown/opts.lua
+++ .config/nvim/after/ftplugin/markdown/opts.lua
@@ -3,6 +3,11 @@ vim.bo.cindent = false
 vim.bo.smartindent = false
 vim.bo.commentstring = '<!-- %s -->'
 
+vim.wo.conceallevel = 2
+vim.wo.concealcursor = 'c'
+
+vim.wo.foldminlines = 1
+
 ---Don't join title/first line of list item with previous lines when yanking
 ---with joined paragraphs
 ---@param line string
