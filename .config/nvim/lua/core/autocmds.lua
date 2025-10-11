607f8fc9d721892462d6b9206bc4aab2118a86a9
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-10-11 07:40:08 -0300

    X

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -581,27 +581,6 @@ do
   local colors_file =
     vim.fs.joinpath(vim.fn.stdpath('state') --[[@as string]], 'colors.json')
 
-  ---@param colors_name string
-  ---@return nil
-  local function load_colorscheme(colors_name)
-    local colors_path = vim.fs.joinpath(
-      vim.fn.stdpath('config') --[[@as string]],
-      'colors',
-      colors_name .. '.lua'
-    )
-    if vim.fn.filereadable(colors_path) == 1 then
-      dofile(colors_path)
-      vim.schedule(function()
-        vim.api.nvim_exec_autocmds('ColorScheme', {})
-      end)
-    else
-      vim.cmd.colorscheme({
-        args = { colors_name },
-        mods = { emsg_silent = true },
-      })
-    end
-  end
-
   ---Restore dark/light background and colorscheme from json so that nvim
   ---'remembers' the background and colorscheme when it is restarted.
   local function restore_colorscheme()
@@ -611,7 +590,10 @@ do
       vim.go.bg = c.bg
     end
     if c.colors_name and c.colors_name ~= vim.g.colors_name then
-      load_colorscheme(c.colors_name)
+      vim.cmd.colorscheme({
+        args = { c.colors_name },
+        mods = { emsg_silent = true },
+      })
     end
   end
 
@@ -641,21 +623,26 @@ do
         vim.schedule(function()
           local json_utils = require('utils.json')
 
-          local d = json_utils.read(colors_file)
-          if d.colors_name == vim.g.colors_name and d.bg == vim.go.bg then
+          local c = json_utils.read(colors_file)
+          if c.colors_name == vim.g.colors_name and c.bg == vim.go.bg then
             return
           end
 
-          if d.colors_name ~= vim.g.colors_name then
-            d.colors_name = vim.g.colors_name
-            pcall(vim.system, { 'setcolor', vim.g.colors_name })
-          end
-          if d.bg ~= vim.go.bg and vim.go.termguicolors then
-            d.bg = vim.go.bg
-            pcall(vim.system, { 'setbg', vim.go.bg })
+          if c.colors_name ~= vim.g.colors_name then
+            c.colors_name = vim.g.colors_name
+            if vim.fn.executable('setcolor') == 1 then
+              vim.system({ 'setcolor', vim.g.colors_name })
+            end
           end
 
-          json_utils.write(colors_file, d)
+          if c.bg ~= vim.go.bg and vim.go.termguicolors then
+            c.bg = vim.go.bg
+            if vim.fn.executable('setbg') == 1 then
+              vim.system({ 'setbg', vim.go.bg })
+            end
+          end
+
+          json_utils.write(colors_file, c)
         end)
       end,
     },
