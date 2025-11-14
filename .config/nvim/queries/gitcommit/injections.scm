e7971ba4ce50c168636e52dbde5bff668385c742
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-11-13 22:15:50 -0300

    X

--- .config/nvim/queries/gitcommit/injections.scm
+++
@@ -1,4 +0,0 @@
-; Inject text language for word-level Git keyword highlighting
-; Text parser should tokenize individual words
-((message_line) @injection.content
- (#set! injection.language "markdown"))
