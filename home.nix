54b2601bf03e1e86d8e5288ca46695bf06f9044c
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-11-27 20:52:13 -0300

    X

--- home.nix
+++ home.nix
@@ -47,7 +47,7 @@ in {
     ".config/nvim/" = mkSymlink ".config/nvim/";
     ".config/pandoc/" = mkSymlink ".config/pandoc/";
     ".config/rg/" = mkSymlink ".config/rg/";
-    ".config/rofig/" = mkSymlink ".config/rofig/";
+    ".config/rofi/" = mkSymlink ".config/rofi/";
     ".config/tmux/" = mkSymlink ".config/tmux/";
     ".config/zathura/" = mkSymlink ".config/zathura/";
     ".bin/" = mkSymlink ".bin/";
@@ -70,6 +70,81 @@ in {
   services.picom = {
     enable = true;
     backend = "glx";
+    shadow = true;
+
+    shadowOpacity = 0.75;
+    shadowOffsets = [(-5) (-5)];
+    shadowExclude = [
+      "! name~=''"
+      "name = 'Notification'"
+      "name = 'Plank'"
+      "name = 'Docky'"
+      "name = 'Kupfer'"
+      "name = 'xfce4-notifyd'"
+      "name = 'cpt_frame_window'"
+      "name *= 'VLC'"
+      "name *= 'compton'"
+      "name *= 'picom'"
+      "name *= 'Chromium'"
+      "name *= 'Chrome'"
+      "class_g = 'Firefox' && argb"
+      "class_g = 'Conky'"
+      "class_g = 'Kupfer'"
+      "class_g = 'Synapse'"
+      "class_g ?= 'Notify-osd'"
+      "class_g ?= 'Cairo-dock'"
+      "class_g ?= 'Xfce4-notifyd'"
+      "class_g ?= 'Xfce4-power-manager'"
+      "_GTK_FRAME_EXTENTS@:c"
+      "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
+    ];
+
+    inactiveOpacity = 1.0;
+    activeOpacity = 1.0;
+
+    fade = true;
+    fadeDelta = 4;
+    fadeSteps = [0.33 0.03];
+
+    vSync = true;
+
+    wintypes = {
+      "tooltip" = {
+        fade = true;
+        shadow = false;
+        opacity = 0.85;
+        focus = true;
+      };
+    };
+
+    settings = {
+      shadow-radius = 12;
+      xinerama-shadow-crop = true;
+
+      frame-opacity = 1;
+      inactive-opacity-override = false;
+      detect-client-opacity = true;
+
+      blur-backgroupnd = true;
+      blur-background-frame = true;
+      blur-background-fixed = false;
+      blur-background-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+      ];
+
+      mark-wmwin-focused = true;
+      mark-ovredir-focused = true;
+      use-ewmh-active-win = true;
+      detect-rounded-corners = true;
+
+      dbe = false;
+      unredir-if-possible = false;
+      detect-transient = true;
+      detect-client-leader = true;
+
+      xrender-sync-fence = true;
+    };
   };
 
   services.kdeconnect = {
