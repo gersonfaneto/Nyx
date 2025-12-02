7c2b9fb1b49729759d1e331e290e0453533bb2f0
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-12-02 19:23:53 -0300

    X

---
+++ .config/nvim/lua/pack/specs/opt/conjure.lua
@@ -0,0 +1,10 @@
+---@type pack.spec
+return {
+  src = 'https://github.com/Olical/conjure',
+  data = {
+    preload = function()
+      vim.g['conjure#filetypes'] = { 'clojure' }
+      vim.g['conjure#mapping#doc_word'] = false
+    end,
+  },
+}
---
+++ .config/nvim/lua/pack/specs/opt/ghcid.lua
@@ -0,0 +1,11 @@
+---@type pack.spec
+return {
+  src = 'https://github.com/ndmitchell/ghcid',
+  data = {
+    postload = function()
+      -- TODO: This is kinda ugly...
+      local plugin = vim.pack.get({ 'ghcid' })[1] --[[@as vim.pack.PlugData]]
+      vim.opt.rtp:append(plugin.path .. '/plugins/nvim/')
+    end,
+  },
+}
