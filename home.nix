c57e494091903e75b88419f148bae6a0203520d1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-12-03 16:07:47 -0300

    X

--- home.nix
+++ home.nix
@@ -33,7 +33,6 @@ in {
   home.file = {
     ".config/alacritty/" = mkSymlink ".config/alacritty/";
     ".config/beets/" = mkSymlink ".config/beets/";
-    ".config/doom/" = mkSymlink ".config/doom/";
     ".config/dunst/" = mkSymlink ".config/dunst/";
     ".config/fastfetch/" = mkSymlink ".config/fastfetch/";
     ".config/fd/" = mkSymlink ".config/fd/";
