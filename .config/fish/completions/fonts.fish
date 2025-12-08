9ed76fde4474ddca06e4be1056310ba95429380e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-12-08 07:21:25 -0300

    X

--- .config/fish/completions/fonts.fish
+++ .config/fish/completions/fonts.fish
@@ -1,9 +1,9 @@
 function __fish_complete_fonts
-    ghostty +list-fonts | grep -v '^[[:blank:]]' | grep -v '^$' | sort | uniq
+    fonts --list --filter | sed 's/\x1b\[[0-9;]*m//g' | sed 's/ (current)$//'
 end
 
 complete -f -c fonts -a '(__fish_complete_fonts)'
 
 complete -f -c fonts -l list -s l -d 'List all the available fonts'
-complete -f -c fonts -l filter -s f -d 'Filter out ignored fonts when listing'
+complete -f -c fonts -l filter -s f -d 'Filter out ignored fonts (only applies with --list)'
 complete -f -c fonts -l help -s h -d 'Show the help message'
