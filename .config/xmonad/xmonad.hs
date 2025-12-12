cc9a595400c74b5a8aaef12a71bd7e4a6af34e11
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-12-12 07:27:37 -0300

    X

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,43 +1,44 @@
-import Data.Map qualified as Map
-import Data.Maybe qualified as Maybe
+import qualified Data.Map                     as Map
+import qualified Data.Maybe                   as Maybe
 
-import XMonad
-import XMonad.Actions.CycleWS
-import XMonad.Actions.Submap
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.InsertPosition
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers (doCenterFloat, doRectFloat)
-import XMonad.Hooks.StatusBar
-import XMonad.Hooks.StatusBar.PP
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Renamed
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.Spacing
-import XMonad.Layout.Spiral
-import XMonad.StackSet qualified as StackSet
-import XMonad.Util.EZConfig (additionalKeysP)
-import XMonad.Util.Loggers
-import XMonad.Util.SpawnOnce
+import           XMonad
+import           XMonad.Actions.CycleWS
+import           XMonad.Actions.Submap
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.EwmhDesktops
+import           XMonad.Hooks.InsertPosition
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers   (doCenterFloat, doRectFloat)
+import           XMonad.Hooks.StatusBar
+import           XMonad.Hooks.StatusBar.PP
+import           XMonad.Layout.NoBorders
+import           XMonad.Layout.Renamed
+import           XMonad.Layout.ResizableTile
+import           XMonad.Layout.Spacing
+import           XMonad.Layout.Spiral
+import qualified XMonad.StackSet              as StackSet
+import           XMonad.Util.EZConfig         (additionalKeysP)
+import           XMonad.Util.Loggers
+import           XMonad.Util.SpawnOnce
+import           XMonad.Util.WindowProperties
 
-import Data.Map qualified as M
-import System.IO (writeFile)
+import qualified Data.Map                     as M
+import           System.IO                    (writeFile)
 
-import Text.Regex.Posix ((=~))
+import           Text.Regex.Posix             ((=~))
 
 q ~? x = fmap (=~ x) q
 
 cBackground = "#002B36"
 cForeground = "#657B83"
-cBlack = "#32344A"
-cBlack' = "#444B6A"
-cRed = "#DC322F"
-cGreen = "#859900"
-cYellow = "#B58900"
-cBlue = "#268BD2"
-cMagenta = "#D33682"
-cCyan = "#2AA198"
+cBlack      = "#32344A"
+cBlack'     = "#444B6A"
+cRed        = "#DC322F"
+cGreen      = "#859900"
+cYellow     = "#B58900"
+cBlue       = "#268BD2"
+cMagenta    = "#D33682"
+cCyan       = "#2AA198"
 
 mSpacing = spacingWithEdge 3
 
@@ -78,15 +79,17 @@ mFloat = doRectFloat (StackSet.RationalRect 0.15 0.15 0.7 0.7)
 
 mManageHook =
     composeAll
-        [ className =? "Navigator" --> doFloat
-        , className =? "Dunst" --> doFloat
+        [ className =? "Dunst" --> doFloat
         , className =? "wiremix" --> mFloat
         , className =? "Qalculate-gtk" --> mFloat
+        , className =? "Brave-browser" --> doShift "1"
         , className =? "firefox" --> doShift "1"
         , className =? "Alacritty" --> doShift "2"
+        , className =? "com.mitchellh.ghostty" --> doShift "2"
         , className =? "Emacs" --> doShift "10"
         , className ~? "^Minecraft*" --> doShift "5"
         , className =? "PrismLauncher" --> mFloat <> doShift "5"
+        , stringProperty "WM_WINDOW_ROLE" ~? "[pop-up|Popup]" --> doFloat
         ]
         <+> insertPosition Below Newer
 
@@ -117,6 +120,7 @@ openSubmap =
             [ ((0, xK_e), spawn mEditor)
             , ((0, xK_b), spawn mBrowser)
             , ((0, xK_q), spawn mCalc)
+            , ((0, xK_z), spawn "boomer")
             ]
 
 rofiSubmap =
@@ -160,8 +164,6 @@ workspaceSubmap =
             , ((0, xK_s), sendMessage $ JumpToLayout "Spiral")
             ]
 
-------------------------------------------------------------------------
-
 mKeys =
     [ ("M-<Escape>", spawn "rofi-system")
     , ("M-<Return>", spawn mTerminal)
@@ -171,7 +173,6 @@ mKeys =
     , ("M-s", systemSubmap)
     , ("M-m", mediaSubmap)
     , ("M-w", workspaceSubmap)
-    , ("M-t z", spawn "boomer")
     , ("M-<F1>", spawn "wallpaper --select")
     , ("M-S-<F1>", spawn "wallpaper --default")
     , ("M-<F2>", spawn "background alt")
@@ -209,8 +210,6 @@ mKeys =
     | (tag, key) <- zip mWorkspaces "1234567890"
     , (action, mask) <- [(StackSet.greedyView, ""), (StackSet.shift, "S-")]
     ]
-
-------------------------------------------------------------------------
 
 toggleFloat w =
     windows
