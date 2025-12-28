f67386b7d0e843728e83179af5bb4a7381cdf5d1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-12-28 20:17:23 -0300

    X

---
+++ .config/nvim/lua/pack/specs/opt/haskell-tools.lua
@@ -0,0 +1,18 @@
+---@type pack.spec
+return {
+  src = 'https://github.com/mrcjkb/haskell-tools.nvim',
+  version = vim.version.range('^6'),
+  data = {
+    init = function()
+      vim.g.haskell_tools = {
+        hls = {
+          settings = {
+            haskell = {
+              formattingProvider = 'stylish-haskell',
+            },
+          },
+        },
+      }
+    end,
+  },
+}
