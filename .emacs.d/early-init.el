1ed7596cc4d668466b3d72d921d673bf3eae9088
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-12-30 15:13:15 -0300

    X

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -2,17 +2,9 @@
 (menu-bar-mode   0)
 (scroll-bar-mode 0)
 
-(setq inhibit-startup-screen t)
-
-(setq use-dialog-box nil)
-
-(setq use-short-answers t)
-
-(setq make-backup-files nil)
-
-(setq auto-save-default nil)
-
 (column-number-mode 1)
 (show-paren-mode    1)
+
+(setq inhibit-startup-screen t)
 
 (load-theme 'modus-vivendi t)
---
+++ .emacs.d/init.el
@@ -0,0 +1,6 @@
+(setq use-dialog-box nil
+      use-short-answers t)
+
+(setq make-backup-files nil
+      auto-save-default nil)
+
