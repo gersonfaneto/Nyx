32c55f2ee6f0a0ac32af946e855a7c3158fe23e1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2026-01-09 09:52:13 -0300

    X

--- .config/nvim/lua/pack/specs/opt/diffview.lua
+++ .config/nvim/lua/pack/specs/opt/diffview.lua
@@ -3,11 +3,25 @@ return {
   src = 'https://github.com/sindrets/diffview.nvim',
   data = {
     deps = {
-      src = 'https://github.com/nvim-lua/plenary.nvim',
+      'https://github.com/nvim-lua/plenary.nvim',
     },
     cmds = {
       'DiffviewOpen',
       'DiffviewFileHistory',
+    },
+    keys = {
+      {
+        lhs = '<Leader>dv',
+        opts = { desc = 'Open diff view' },
+      },
+      {
+        lhs = '<Leader>dh',
+        opts = { desc = 'Open file diff history' },
+      },
+      {
+        lhs = '<Leader>dc',
+        opts = { desc = 'Close diff view' },
+      },
     },
     postload = function()
       local dv = require('diffview')
@@ -25,6 +39,12 @@ return {
         --   },
         -- },
       })
+
+      -- stylua: ignore start
+      vim.keymap.set({'n'}, '<Leader>dv', '<Cmd>DiffviewOpen<CR>', { desc = 'Open diff view' })
+      vim.keymap.set({'n'}, '<Leader>dh', '<Cmd>DiffviewFileHistory<CR>', { desc = 'Open file diff history' })
+      vim.keymap.set({'n'}, '<Leader>dc', '<Cmd>DiffviewClose<CR>', { desc = 'Close diff view' })
+      -- stylua: ignore end
     end,
   },
 }
