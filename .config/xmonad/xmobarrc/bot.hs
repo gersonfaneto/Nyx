10d508e5be2d96139e2588f4d7103298c8576fe1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2026-02-04 17:28:46 -0300

    X

--- .config/xmonad/xmobarrc/bot.hs
+++ .config/xmonad/xmobarrc/bot.hs
@@ -1,27 +1,27 @@
 Config
-  { font = "xft:Aporetic Sans Mono:style=Regular:size=8:antialias=true"
-    bgColor = "#0B0806",
-    fgColor = "#A19782",
-    alpha = 255,
-    position = BottomHM 30 10 10 0 10,
-    iconOffset = -1,
-    lowerOnStart = True,
-    hideOnStart = False,
-    allDesktops = True,
-    overrideRedirect = True,
-    pickBroadest = False,
-    persistent = True,
-    border = FullBM 0,
-    borderColor = "#A19782",
-    borderWidth = 1,
-    iconRoot = ".",
-    commands =
+  { font = "Aporeti Serif Mono Bold 10"
+  , bgColor = "#0B0806"
+  , fgColor = "#A19782"
+  , alpha = 255
+  , position = BottomHM 30 10 10 0 10
+  , iconOffset = -1
+  , lowerOnStart = True
+  , hideOnStart = False
+  , allDesktops = True
+  , overrideRedirect = True
+  , pickBroadest = False
+  , persistent = True
+  , border = FullBM 0
+  , borderColor = "#A19782"
+  , borderWidth = 1
+  , iconRoot = "."
+  , commands =
       [ Run ComX "player" [] "" "player" 20
       , Run ComX "dunststatus" [] "" "dunst" 20
       , Run Volume "default" "Master" [ "-t", "<fn=1><status></fn><volume>%", "--", "--on", "󰕾 ", "--off", "󰖁 ", "--onc", "#a19782", "--offc", "#a19782" ] 10
       , Run Battery [ "-t", "<fn=3><acstatus></fn> <left>%", "--", "-i", "", "-O", "", "-o", "" ] 10
-      ],
-    sepChar = "%",
-    alignSep = "}{",
-    template = " %battery% } %player% { %dunst%  %default:Master% "
+      ]
+  , sepChar = "%",
+  , alignSep = "}{",
+  , template = "  %battery% } %player% { %dunst%  %default:Master%  "
   }
--- .config/xmonad/xmobarrc/top.hs
+++ .config/xmonad/xmobarrc/top.hs
@@ -1,27 +1,27 @@
 Config
-    { font = "xft:Aporetic Sans Mono:style=Regular:size=8:antialias=true"
-    , bgColor = "#0B0806"
-    , fgColor = "#A19782"
-    , alpha = 255
-    , position = TopHM 30 10 10 10 0
-    , iconOffset = -1
-    , lowerOnStart = True
-    , hideOnStart = False
-    , allDesktops = True
-    , overrideRedirect = True
-    , pickBroadest = False
-    , persistent = True
-    , border = FullBM 0
-    , borderColor = "#A19782"
-    , borderWidth = 1
-    , iconRoot = "."
-    , commands =
-        [ Run Date "%d.%m.%y / %A / %H:%M" "date" 10
-        , Run ComX "caffeinatestatus" [] "" "caffeine" 20
-        , Run XPropertyLog "_XMONAD_LOG_1"
-        , Run Com "cat" ["/home/gerson/.cache/xmonad-submap"] "submap" 1
-        ]
-    , sepChar = "%"
-    , alignSep = "}{"
-    , template = " %_XMONAD_LOG_1% } %submap% { %caffeinatestatus%  %date% "
-    }
+  { font = "Aporetic Serif Mono Bold 10"
+  , bgColor = "#0B0806"
+  , fgColor = "#A19782"
+  , alpha = 255
+  , position = TopHM 30 10 10 10 0
+  , iconOffset = -1
+  , lowerOnStart = True
+  , hideOnStart = False
+  , allDesktops = True
+  , overrideRedirect = True
+  , pickBroadest = False
+  , persistent = True
+  , border = FullBM 0
+  , borderColor = "#A19782"
+  , borderWidth = 1
+  , iconRoot = "."
+  , commands =
+      [ Run Date "%d.%m.%y / %A / %H:%M" "date" 10
+      , Run ComX "caffeinatestatus" [] "" "caffeine" 20
+      , Run XPropertyLog "_XMONAD_LOG_1"
+      , Run Com "cat" ["/home/gerson/.cache/xmonad-submap"] "submap" 1
+      ]
+  , sepChar = "%"
+  , alignSep = "}{"
+  , template = "  %_XMONAD_LOG_1% } %submap% { %caffeinatestatus%  %date%  "
+  }
