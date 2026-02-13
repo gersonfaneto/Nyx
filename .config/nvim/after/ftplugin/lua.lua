34d94ca7a05218dbf4a0c213949be6100dd0abec
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2026-02-13 19:01:36 -0300

    X

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -2,4 +2,3 @@
 vim.keymap.set('n', '<leader>xf', '<CMD>source %<CR>', { desc = 'Source current file' })
 vim.keymap.set({ 'n', 'x' }, '<Leader>xe', ':.lua<CR>', { desc = 'Source current selection' })
 -- stylua: ignore end
-
---
+++ .config/nvim/lua/pack/specs/opt/conjure.lua
@@ -0,0 +1,11 @@
+---@type minimal.pack.spec
+return {
+  src = 'https://github.com/Olical/conjure',
+  data = {
+    preload = function()
+      vim.g['conjure#filetypes'] = { 'clojure' }
+      vim.g['conjure#mapping#prefix'] = ',c'
+      vim.g['conjure#mapping#doc_word'] = false
+    end,
+  },
+}
