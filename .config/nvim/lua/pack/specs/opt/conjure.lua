72aeb27dd2d5855e6a4bfd52d0bb897760555459
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2026-02-14 10:57:55 -0300

    X

--- .config/nvim/lua/pack/specs/opt/conjure.lua
+++ .config/nvim/lua/pack/specs/opt/conjure.lua
@@ -2,21 +2,6 @@
 return {
   src = 'https://github.com/Olical/conjure',
   data = {
-    deps = {
-      {
-        src = 'https://github.com/saghen/blink.cmp',
-      },
-      {
-        src = 'https://github.com/saghen/blink.compat',
-        data = {
-          deps = {
-            {
-              src = 'https://github.com/PaterJason/cmp-conjure',
-            },
-          },
-        },
-      },
-    },
     events = {
       event = 'FileType',
       pattern = 'clojure',
@@ -27,22 +12,6 @@ return {
       vim.g['conjure#mapping#doc_word'] = false
     end,
     postload = function()
-      require('blink.cmp').setup({
-        sources = {
-          default = {
-            'conjure',
-          },
-          provides = {
-            conjure = {
-              name = 'conjure',
-              module = 'blink.compat.source',
-              score_offset = -3,
-            },
-          },
-        },
-        appearance = { kind_icons = { Conjure = '🪄' } },
-      })
-
       require('which-key').add({
         { '<LocalLeader>c', group = 'Conjure' },
       })
